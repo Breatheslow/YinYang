a3cf167a09e6ed0dd7634a11abe21ccea3a65c8a
Breatheslow <92402674+Breatheslow@users.noreply.github.com>
2021-10-21 12:44:53 +0200

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,6 @@
 (function()
 {
-  var image = '<img class="IntroImage" src="images/Yin.png">>';
+  var image = '<img class="IntroImage" src="../images/Yin.png">>';
   $('body').hide();
   $('html').append(image);
   $('.IntroImage').css({"width": "50vw",	"height": "50vw", "margin": "auto", "display" :"block"});
