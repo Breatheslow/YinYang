668c9b134083bccac53b9e0bea4978a19fe5f27c
chaxchaptatwo <chax.chapta@gmail.com>
2021-10-21 12:43:11 +0200

    intro symbol

---
+++ javascript/javascript.js
@@ -0,0 +1,8 @@
+(function()
+{
+  var image = '<img class="IntroImage" src="images/Yin.png">>';
+  $('body').hide();
+  $('html').append(image);
+  $('.IntroImage').css({"width": "50vw",	"height": "50vw", "margin": "auto", "display" :"block"});
+
+}());
